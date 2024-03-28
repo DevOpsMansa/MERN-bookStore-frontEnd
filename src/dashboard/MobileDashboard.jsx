806bbde82f37d80e5c3fd4344ccba2e057135d69
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-03-28 01:36:22 -0500

    Twenty-forth commit: Completed an upload a book on the dashboard

---
+++ src/dashboard/MobileDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MobileDashboard = () => {
+  return (
+    <div>MobileDashboard</div>
+  )
+}
+
+export default MobileDashboard
--- src/dashboard/SideBar.jsx
+++ src/dashboard/SideBar.jsx
@@ -1,48 +1,114 @@
 import { Sidebar } from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards} from "react-icons/hi";
+import { HiSupport, HiArrowSmRight, HiChartPie, HiInbox, HiOutlineCloudUpload, HiShoppingBag, HiTable, HiUser, HiViewBoards} from "react-icons/hi";
+import Img from "../assets/profile.jpg"
+import MobileDashboard from "./MobileDashboard";
+
 
 
 const SideBar = () => {
   return (
-    <Sidebar aria-label="Sidebar with content separator example">
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Kanban
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox}>
-            Inbox
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-            Sign Up
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Upgrade to Pro
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Documentation
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={BiBuoy}>
-            Help
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+    // used Flowbite React - https://www.flowbite-react.com/docs/components/sidebar 
+    <div className=''>
+      <Sidebar aria-label="Sidebar with content separator example" className='hidden md:block'>
+        <Sidebar.Logo
+          href="/"
+          img={Img}
+          className='w-10 h-10 rounded-full'
+          imgAlt="Flowbite logo"
+        >
+          
+        </Sidebar.Logo>
+        <Sidebar.Items>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item
+              href="/admin/dashboard"
+              icon={HiChartPie}>
+              <p>
+                Dashboard
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/admin/dashboard/upload"
+              icon={HiOutlineCloudUpload}
+            >
+              <p>
+                Upload Book
+              </p>
+            </Sidebar.Item>
+
+            <Sidebar.Item
+              href="/admin/dashboard/manage"
+              icon={HiInbox}
+            >
+              <p>
+                ManageBooks
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="#"
+              icon={HiUser}
+            >
+              <p>
+                Users
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="#"
+              icon={HiShoppingBag}
+            >
+              <p>
+                Products
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/login"
+              icon={HiArrowSmRight}
+            >
+              <p>
+                Sign In
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="/logout"
+              icon={HiTable}
+            >
+              <p>
+                Log out
+              </p>
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+          <Sidebar.ItemGroup>
+            <Sidebar.Item
+              href="#"
+              icon={HiChartPie}
+            >
+              <p>
+                Upgrade to Pro
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="#"
+              icon={HiViewBoards}
+            >
+              <p>
+                Documentation
+              </p>
+            </Sidebar.Item>
+            <Sidebar.Item
+              href="#"
+              icon={HiSupport}
+            >
+              <p>
+                Help
+              </p>
+            </Sidebar.Item>
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+      </Sidebar>
+      <div className='md:hidden'>
+          <MobileDashboard/>
+      </div>
+    </div>
   )
 }
 
